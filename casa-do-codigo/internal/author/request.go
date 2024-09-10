f5a74cde1f9e57e32481927bd0af5d0e9d6dc559
mpedroni <mpedroni77@gmail.com>
2024-09-10 19:52:57 -0300

    update create author request constraints

--- casa-do-codigo/internal/author/request.go
+++ casa-do-codigo/internal/author/request.go
@@ -2,6 +2,6 @@ package author
 
 type CreateAuthorRequest struct {
 	Name        string `json:"name" binding:"required"`
-	Email       string `json:"email"`
-	Description string `json:"description"`
+	Email       string `json:"email" binding:"required,email"`
+	Description string `json:"description" binding:"required,max=400"`
 }
